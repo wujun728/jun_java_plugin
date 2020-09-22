ce235f6adca20a40d1193a8ea220f437f9b3a8ca
wujun728 <wujun728@hotmail.com>
2020-09-23 01:10:49 +0800

    Activiti5，待补充

--- jun_activiti/src/main/java/com/java1234/activiti/table/ActivitiTest01.java
+++ jun_activiti/src/main/java/com/java1234/activiti/table/ActivitiTest01.java
@@ -6,34 +6,24 @@ import org.junit.Test;
 
 public class ActivitiTest01 {
 
-	/**
-	 * ����Activiti��Ҫ��25��
-	 */
+	 
 	@Test
 	public void testCreateTable(){
-		ProcessEngineConfiguration pec=ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration(); // ��ȡ������������
-		pec.setJdbcDriver("com.mysql.jdbc.Driver"); // ��������
-		pec.setJdbcUrl("jdbc:mysql://localhost:3306/activiti5"); // �������ӵ�ַ
+		ProcessEngineConfiguration pec=ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();  
+		pec.setJdbcDriver("com.mysql.jdbc.Driver"); 
+		pec.setJdbcUrl("jdbc:mysql://localhost:3306/activiti5"); 
 		pec.setJdbcUsername("root"); // �û���
 		pec.setJdbcPassword("mysqladmin"); // ����
-		
-		/**
-		 * ����ģʽ  true �Զ������͸��±�
-		 */
+		 
 		pec.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
 		
-		// ��ȡ�����������
 		ProcessEngine pe=pec.buildProcessEngine(); 
 	}
 	
-	/**
-	 * ����Activiti��Ҫ��25�� ʹ�������ļ�
-	 */
+	 
 	@Test
 	public void testCreateTableWithXml(){
-		 // ��������
 	    ProcessEngineConfiguration pec=ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
-	    // ��ȡ�����������
 	    ProcessEngine processEngine=pec.buildProcessEngine();
 	}
 }
