41decbdcebfe438eed84c5700d065f4cd718ed13
wujun728 <wujun728@hotmail.com>
2020-09-27 01:37:40 +0800

    jun_dbtracer数据追踪，监控系统中标修改前后的数据及记录，基于mybatis插件机制实现

---
+++ jun_dbtracer/dbtracer_core/src/main/java/com/jun/plugin/dbtracer/sql/SQLParseResult.java
@@ -0,0 +1,73 @@
+package com.jun.plugin.dbtracer.sql;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SQLParseResult {
+
+    private String table;
+    private String querySql;
+    private List<String> updateColume;
+
+    private Map<Integer, String> updateColIdx2Name;
+
+    private String where;
+
+    private String key;
+
+    SQLParseResult() {
+        updateColume = new ArrayList<String>();
+        updateColIdx2Name = new HashMap<Integer, String>();
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getQuerySql() {
+        return querySql;
+    }
+
+    public void setQuerySql(String querySql) {
+        this.querySql = querySql;
+    }
+
+    public List<String> getUpdateColume() {
+        return updateColume;
+    }
+
+    public void setUpdateColume(List<String> updateColume) {
+        this.updateColume = updateColume;
+    }
+
+    public Map<Integer, String> getUpdateColIdx2Name() {
+        return updateColIdx2Name;
+    }
+
+    public void setUpdateColIdx2Name(Map<Integer, String> updateColIdx2Name) {
+        this.updateColIdx2Name = updateColIdx2Name;
+    }
+
+    public String getWhere() {
+        return where;
+    }
+
+    public void setWhere(String where) {
+        this.where = where;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+}
