f677c3118b403661d7ff75f3a7ac692a562a9ffd
wujun728 <wujun728@hotmail.com>
2021-06-26 02:16:58 +0800

    1

--- springboot_plugin/springboot_data_mybatis/spring-data-mybatis/src/main/java/org/springframework/data/mybatis/repository/query/MybatisEntityMetadata.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.data.mybatis.repository.query;
-
-import org.springframework.data.repository.core.EntityMetadata;
-
-public interface MybatisEntityMetadata<T> extends EntityMetadata<T> {
-
-	String getEntityName();
-
-}
