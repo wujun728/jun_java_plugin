8e4bf765b9f864fc8a969c11d721e9d7e6f9501f
wujun728 <wujun728@hotmail.com>
2021-07-13 00:53:09 +0800

    1

---
+++ java_plugin/jun_email/src/main/java/com/jun/plugin/me/SendMailException.java
@@ -0,0 +1,21 @@
+package com.jun.plugin.me;
+
+/**
+ * Send Email Exception
+ *
+ * @author biezhi
+ * @date 2018/10/9
+ */
+public class SendMailException extends Exception {
+
+    public SendMailException() {
+    }
+
+    public SendMailException(String message) {
+        super(message);
+    }
+
+    public SendMailException(Throwable cause) {
+        super(cause);
+    }
+}
