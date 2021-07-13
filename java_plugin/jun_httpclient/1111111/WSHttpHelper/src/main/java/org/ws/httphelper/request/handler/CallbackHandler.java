81583b32a5e8a716ee17f2ce2860fd7cd5860325
wujun728 <wujun728@hotmail.com>
2021-07-14 02:58:00 +0800

    1

--- java_plugin/jun_httpclient/1111111/WSHttpHelper/src/main/java/org/ws/httphelper/request/handler/CallbackHandler.java
+++
@@ -1,8 +0,0 @@
-package org.ws.httphelper.request.handler;
-
-import org.ws.httphelper.exception.WSException;
-import org.ws.httphelper.model.ResponseResult;
-
-public interface CallbackHandler {
-    public ResponseResult execute(ResponseResult result) throws WSException;
-}
--- java_plugin/jun_httpclient/httpclientutil/src/test/java/com/arronlong/httpclientutil/test/httpConn.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:count
-echo|set /p="Num of HTTP Connections(port:80): " & netstat -na |findstr "ESTABLISHED" | find /C ":80"
-timeout 2 > nul
-GOTO count
