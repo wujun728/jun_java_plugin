4311d73f173bcc6388ce62aae25ab81c80f0c491
wujun728 <wujun728@hotmail.com>
2021-07-14 22:29:27 +0800

    1

--- java_plugin/jun_jsoup/jsoup-learning/src/main/java/org/jsoup/parser/ITokeniserState.java
+++
@@ -1,9 +0,0 @@
-package org.jsoup.parser;
-
-/**
- * @author code4crafter@gmail.com
- */
-interface ITokeniserState {
-
-    abstract void read(Tokeniser t, CharacterReader r);
-}
--- java_plugin/jun_jsoup/jsoup-learning/src/main/java/us/codecraft/learning/automata/ABStateMachine.java
+++
@@ -1,9 +0,0 @@
-package us.codecraft.learning.automata;
-
-/**
- * @author code4crafter@gmail.com
- */
-public interface ABStateMachine {
-
-    void process(StringReader reader) throws StringReader.EOFException;
-}
