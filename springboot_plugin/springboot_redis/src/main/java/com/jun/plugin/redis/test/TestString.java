00ee81273831ab1a82be689528d7fcc878619373
wujun728 <wujun728@hotmail.com>
2021-07-21 21:56:53 +0800

    1

---
+++ springboot_plugin/springboot_redis/src/main/java/com/jun/plugin/redis/test/TestString.java
@@ -0,0 +1,9 @@
+package com.jun.plugin.redis.test;
+
+public class TestString {
+
+    public static void main(String[] args) {
+        String a="c";
+        System.out.println(a.charAt(a.length()-1));
+    }
+}
