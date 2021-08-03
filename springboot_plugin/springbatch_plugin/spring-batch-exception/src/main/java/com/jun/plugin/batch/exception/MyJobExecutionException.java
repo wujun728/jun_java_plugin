1d6e6248e0a94a4a3532b25d2c16467eac601f77
wujun728 <wujun728@hotmail.com>
2021-08-03 11:22:46 +0800

    1

---
+++ springboot_plugin/springbatch_plugin/spring-batch-exception/src/main/java/com/jun/plugin/batch/exception/MyJobExecutionException.java
@@ -0,0 +1,13 @@
+package com.jun.plugin.batch.exception;
+
+/**
+ * @author MrBird
+ */
+public class MyJobExecutionException extends  Exception{
+
+    private static final long serialVersionUID = 7168487913507656106L;
+
+    public MyJobExecutionException(String message) {
+        super(message);
+    }
+}
