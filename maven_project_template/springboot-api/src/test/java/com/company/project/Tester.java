9eed2b32fdadd4054b6a7e40d8c584e8f8027ed9
wujun <wujun728@163.com>
2025-01-14 11:51:39 +0800

    111

---
+++ maven_project_template/springboot-api/src/test/java/com/company/project/Tester.java
@@ -0,0 +1,22 @@
+package com.company.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 单元测试
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class Tester {
+
+    @Test
+    public void test() {
+
+    }
+}
+
+
+
