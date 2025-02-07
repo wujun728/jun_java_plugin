9337b199098d5e220befad2f7913c6b4eb03a31f
wujun <wujun728@163.com>
2025-02-07 11:09:01 +0800

    1

---
+++ jun_springcloud_starter/jun-db-activerecord/src/main/java/io/github/wujun728/sql/ApiEngine.java
@@ -0,0 +1,399 @@
+package io.github.wujun728.sql;
+
+import com.alibaba.druid.pool.DruidPooledConnection;
+import com.alibaba.fastjson.JSONObject;
+import io.github.wujun728.db.record.Page;
+import io.github.wujun728.sql.entity.ApiDataSource;
+import io.github.wujun728.sql.entity.ApiSql;
+import io.github.wujun728.sql.entity.DBConfig;
+import io.github.wujun728.sql.entity.SqlWithParam;
+import io.github.wujun728.sql.utils.PoolManager;
+import io.github.wujun728.sql.utils.XmlParser;
+import io.github.wujun728.util.JdbcUtil;
+import io.github.wujun728.sql.engine.DynamicSqlEngine;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ApiEngine {
+
+    private static Logger logger = LoggerFactory.getLogger(ApiEngine.class);
+
+    DynamicSqlEngine dynamicSqlEngine = new DynamicSqlEngine();
+
+    DBConfig dbConfig;
+
+    Map<String, Map<String, ApiSql>> sqlMap = new HashMap<>();
+    Map<String, ApiDataSource> dataSourceMap;
+
+    public ApiEngine(DBConfig dbConfig) {
+        this.dbConfig = dbConfig;
+        initXml();
+    }
+
+    public void initXml() {
+        try {
+            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            Resource[] resources = resolver.getResources(this.dbConfig.getSql());
+
+            for (Resource res : resources) {
+                String filename = res.getFilename();
+                if(filename.equals("ds.xml")){
+                    continue;
+                }
+                InputStream inputStream = res.getInputStream();
+                String content = IOUtils.toString(inputStream, Charsets.toCharset(StandardCharsets.UTF_8));
+                Map<String, ApiSql> stringSqlMap = XmlParser.parseSql(content);
+                this.sqlMap.put(filename.split("\\.")[0], stringSqlMap);
+                logger.info("APISQL register sql xml: {}", filename);
+            }
+            Resource dsResource = resolver.getResource(this.dbConfig.getDatasource());
+
+            String filename = dsResource.getFilename();
+            InputStream inputStream = dsResource.getInputStream();
+            String content = IOUtils.toString(inputStream, Charsets.toCharset(StandardCharsets.UTF_8));
+            this.dataSourceMap = XmlParser.parseDatasource(content);
+            logger.info("APISQL register datasource xml: {}", filename);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+//    public void initApiSql(DataSource dataSource) {
+//        try {
+//            Connection connection = dataSource.getConnection();
+//            String sql_api = " select * from api_sql ";
+//            List<Map<String, Object>>  apiSqls = SqlXmlUtil.query(connection,sql_api);
+//
+//            for(Map map : apiSqls){
+//                BeanUtil.copyToList(FieldUtils)
+//                MapUtil.toCamelCaseMap(map);
+//            }
+//
+//            for (Resource res : resources) {
+//                String filename = res.getFilename();
+//                InputStream inputStream = res.getInputStream();
+//                String content = IOUtils.toString(inputStream, Charsets.toCharset(StandardCharsets.UTF_8));
+//                Map<String, SqlNode> stringSqlMap = XmlParser.parseSql(content);
+//                this.sqlMap.put(filename.split("\\.")[0], stringSqlMap);
+//                logger.info("APISQL register sql xml: {}", filename);
+//            }
+//            Resource dsResource = resolver.getResource(this.dbConfig.getDatasource());
+//
+//            String sql_datasource = " select * from api_datasource ";
+//            List<Map<String, Object>>  apiDS = SqlXmlUtil.query(connection,sql_datasource);
+//
+//
+//            String filename = dsResource.getFilename();
+//            InputStream inputStream = dsResource.getInputStream();
+//            String content = IOUtils.toString(inputStream, Charsets.toCharset(StandardCharsets.UTF_8));
+//            this.dataSourceMap = XmlParser.parseDatasource(content);
+//            logger.info("APISQL register datasource xml: {}", filename);
+//        } catch (Exception e) {
+//            logger.error(e.getMessage(), e);
+//        }
+//    }
+
+
+    public Page<JSONObject> executeQueryPage( String namespace, String sqlId, Map<String, Object> data,int pageNumber,int limit) {
+        Page pageVo = new Page();
+        ApiSql sql = this.sqlMap.get(namespace).get(sqlId);
+        if (!dataSourceMap.containsKey(sql.getDatasourceId())) {
+            throw new RuntimeException("datasource not found : " + sql.getDatasourceId());
+        }
+        data.put("start",(pageNumber-1)*(limit));
+        data.put("end",pageNumber*limit);
+        ApiDataSource dataSource = dataSourceMap.get(sql.getDatasourceId());
+        String pageSql = sql.getText()+ " LIMIT #{start}, #{end} ";
+        SqlMeta sqlMeta = dynamicSqlEngine.parse(pageSql, data);
+        List<JSONObject> results = JdbcUtil.executeQuery(dataSource, sqlMeta.getSql(), sqlMeta.getJdbcParamValues());//executeQuery(namespace, sqlId, data);
+
+        String totalSql = "  select count(1) as count FROM ( "+sql.getText()+" ) AS subquery ";
+        SqlMeta sqlMeta2 = dynamicSqlEngine.parse(totalSql, data);
+
+        Long totalpage = JdbcUtil.executeQueryInt(dataSource, sqlMeta2.getSql(), sqlMeta2.getJdbcParamValues());
+        pageVo.setList(results);
+        pageVo.setTotalRow(Math.toIntExact(totalpage));
+        pageVo.setPageNumber(pageNumber);
+        pageVo.setPageSize(limit);
+        pageVo.setTotalPage(Math.toIntExact(totalpage) / limit);
+        return pageVo;
+    }
+    /**
+     * execute select sql with parameters, return a list of java bean entity
+     *
+     * @param namespace name of xml file
+     * @param sqlId     sql id in <sql> tag
+     * @param data      sql parameters
+     * @param clazz     class of java bean entity
+     * @return
+     */
+    public <T> List<T> executeQueryEntity(String namespace, String sqlId, Map<String, Object> data, Class<T> clazz) {
+        List<JSONObject> list = executeQuery(namespace, sqlId, data);
+
+        List<T> collect = list.stream().map(t -> t.toJavaObject(clazz)).collect(Collectors.toList());
+        return collect;
+    }
+
+    /**
+     * execute select sql with parameters, return a list of JSONObject
+     *
+     * @param namespace name of xml file
+     * @param sqlId     sql id in <sql> tag
+     * @param data      sql parameters
+     * @return
+     */
+    public List<JSONObject> executeQuery(String namespace, String sqlId, Map<String, Object> data) {
+        try {
+            if (!sqlMap.containsKey(namespace)) {
+                throw new RuntimeException("namespace not found : " + namespace);
+            } else {
+                if (!sqlMap.get(namespace).containsKey(sqlId)) {
+                    throw new RuntimeException("sqlId not found : " + sqlId);
+                } else {
+                    ApiSql sql = this.sqlMap.get(namespace).get(sqlId);
+                    if (!dataSourceMap.containsKey(sql.getDatasourceId())) {
+                        throw new RuntimeException("datasource not found : " + sql.getDatasourceId());
+                    }
+                    ApiDataSource dataSource = dataSourceMap.get(sql.getDatasourceId());
+                    SqlMeta sqlMeta = dynamicSqlEngine.parse(sql.getText(), data);
+
+                    return JdbcUtil.executeQuery(dataSource, sqlMeta.getSql(), sqlMeta.getJdbcParamValues());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+    public JSONObject executeQueryOne(String namespace, String sqlId, Map<String, Object> data) {
+        try {
+            if (!sqlMap.containsKey(namespace)) {
+                throw new RuntimeException("namespace not found : " + namespace);
+            } else {
+                if (!sqlMap.get(namespace).containsKey(sqlId)) {
+                    throw new RuntimeException("sqlId not found : " + sqlId);
+                } else {
+                    ApiSql sql = this.sqlMap.get(namespace).get(sqlId);
+                    if (!dataSourceMap.containsKey(sql.getDatasourceId())) {
+                        throw new RuntimeException("datasource not found : " + sql.getDatasourceId());
+                    }
+                    ApiDataSource dataSource = dataSourceMap.get(sql.getDatasourceId());
+                    SqlMeta sqlMeta = dynamicSqlEngine.parse(sql.getText(), data);
+                    return JdbcUtil.executeQueryById(dataSource, sqlMeta.getSql(), sqlMeta.getJdbcParamValues());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+    public int executeQueryCount(String namespace, String sqlId, Map<String, Object> data) {
+        try {
+            if (!sqlMap.containsKey(namespace)) {
+                throw new RuntimeException("namespace not found : " + namespace);
+            } else {
+                if (!sqlMap.get(namespace).containsKey(sqlId)) {
+                    throw new RuntimeException("sqlId not found : " + sqlId);
+                } else {
+                    ApiSql sql = this.sqlMap.get(namespace).get(sqlId);
+                    if (!dataSourceMap.containsKey(sql.getDatasourceId())) {
+                        throw new RuntimeException("datasource not found : " + sql.getDatasourceId());
+                    }
+                    ApiDataSource dataSource = dataSourceMap.get(sql.getDatasourceId());
+                    SqlMeta sqlMeta = dynamicSqlEngine.parse(sql.getText(), data);
+                    Object obj =  JdbcUtil.executeQueryOneColumn(dataSource, sqlMeta.getSql(), sqlMeta.getJdbcParamValues());
+                    if(obj instanceof Integer){
+                        return (int) obj;
+                    }else {
+                        return (Integer) obj;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * execute select sql without parameters, return a list of JSONObject
+     *
+     * @param namespace name of xml file
+     * @param sqlId     sql id in <sql> tag
+     * @return
+     */
+    public List<JSONObject> executeQuery(String namespace, String sqlId) {
+        return executeQuery(namespace, sqlId, null);
+    }
+
+    /**
+     * execute select sql without parameters, return a list of java bean entity
+     *
+     * @param namespace name of xml file
+     * @param sqlId     sql id in <sql> tag
+     * @param clazz     class of java bean entity
+     * @return
+     */
+    public <T> List<T> executeQueryEntity(String namespace, String sqlId, Class<T> clazz) {
+        return executeQueryEntity(namespace, sqlId, null, clazz);
+    }
+
+    /**
+     * execute insert/delete/update sql with parameters
+     *
+     * @param namespace name of xml file
+     * @param sqlId     sql id in <sql> tag
+     * @param data      sql parameters
+     * @return
+     */
+    public int executeUpdate(String namespace, String sqlId, Map<String, Object> data) {
+        try {
+            if (!sqlMap.containsKey(namespace)) {
+                throw new RuntimeException("namespace not found : " + namespace);
+            } else {
+                if (!sqlMap.get(namespace).containsKey(sqlId)) {
+                    throw new RuntimeException("sqlId not found : " + sqlId);
+                } else {
+                    ApiSql sql = this.sqlMap.get(namespace).get(sqlId);
+                    if (!dataSourceMap.containsKey(sql.getDatasourceId())) {
+                        throw new RuntimeException("datasource not found : " + sql.getDatasourceId());
+                    }
+                    ApiDataSource dataSource = dataSourceMap.get(sql.getDatasourceId());
+                    SqlMeta sqlMeta = dynamicSqlEngine.parse(sql.getText(), data);
+
+                    return JdbcUtil.executeUpdate(dataSource, sqlMeta.getSql(), sqlMeta.getJdbcParamValues());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * execute insert/delete/update sql without parameters
+     *
+     * @param namespace name of xml file
+     * @param sqlId     sql id in <sql> tag
+     * @return
+     */
+    public int executeUpdate(String namespace, String sqlId) {
+        return executeUpdate(namespace, sqlId, null);
+    }
+
+    /**
+     * execute select/insert/delete/update sql with parameters
+     *
+     * @param namespace name of xml file
+     * @param sqlId     sql id in <sql> tag
+     * @param data      sql parameters
+     * @return
+     */
+    public Object execute(String namespace, String sqlId, Map<String, Object> data) {
+        try {
+            if (!sqlMap.containsKey(namespace)) {
+                throw new RuntimeException("namespace not found : " + namespace);
+            } else {
+                if (!sqlMap.get(namespace).containsKey(sqlId)) {
+                    throw new RuntimeException("sqlId not found : " + sqlId);
+                } else {
+                    ApiSql sql = this.sqlMap.get(namespace).get(sqlId);
+                    if (!dataSourceMap.containsKey(sql.getDatasourceId())) {
+                        throw new RuntimeException("datasource not found : " + sql.getDatasourceId());
+                    }
+                    ApiDataSource dataSource = dataSourceMap.get(sql.getDatasourceId());
+                    SqlMeta sqlMeta = dynamicSqlEngine.parse(sql.getText(), data);
+
+                    return JdbcUtil.execute(dataSource, sqlMeta.getSql(), sqlMeta.getJdbcParamValues());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * execute select/insert/delete/update sql without parameters
+     *
+     * @param namespace name of xml file
+     * @param sqlId     sql id in <sql> tag
+     * @return
+     */
+    public Object execute(String namespace, String sqlId) {
+        return execute(namespace, sqlId, null);
+    }
+
+    public List<Object> executeWithinTransaction(List<SqlWithParam> sqlList) {
+        DruidPooledConnection connection = null;
+
+        try {
+            String namespace = sqlList.get(0).getNamespace();
+            connection = getConnection(namespace, sqlList.get(0).getSqlId());
+            connection.setAutoCommit(false);
+
+            if (!sqlMap.containsKey(namespace)) {
+                throw new RuntimeException("namespace not found : " + namespace);
+            } else {
+                List<Object> result = new ArrayList<>();
+                for (int i = 0; i < sqlList.size(); i++) {
+                    String sqlId = sqlList.get(i).getSqlId();
+
+                    if (!sqlMap.get(namespace).containsKey(sqlId)) {
+                        throw new RuntimeException("sqlId not found : " + sqlId);
+                    } else {
+                        ApiSql sql = this.sqlMap.get(namespace).get(sqlId);
+                        SqlMeta sqlMeta = dynamicSqlEngine.parse(sql.getText(), sqlList.get(i).getParameters());
+
+                        Object object = JdbcUtil.execute(connection, sqlMeta.getSql(), sqlMeta.getJdbcParamValues());
+                        result.add(object);
+                    }
+                }
+                connection.commit();
+                return result;
+            }
+        } catch (Exception e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private DruidPooledConnection getConnection(String namespace, String sqlId) throws SQLException {
+
+
+        if (!sqlMap.containsKey(namespace)) {
+            throw new RuntimeException("namespace not found : " + sqlId);
+        } else {
+            if (!sqlMap.get(namespace).containsKey(sqlId)) {
+                throw new RuntimeException("sqlId not found : " + sqlId);
+            } else {
+                ApiSql sql = this.sqlMap.get(namespace).get(sqlId);
+                if (!dataSourceMap.containsKey(sql.getDatasourceId())) {
+                    throw new RuntimeException("datasource not found : " + sql.getDatasourceId());
+                }
+                ApiDataSource dataSource = dataSourceMap.get(sql.getDatasourceId());
+                DruidPooledConnection connection = PoolManager.getPooledConnection(dataSource);
+                return connection;
+            }
+        }
+
+    }
+}
---
+++ jun_springcloud_starter/jun-db-activerecord/src/main/java/io/github/wujun728/sql/entity/Result.java
@@ -0,0 +1,188 @@
+package io.github.wujun728.sql.entity;
+
+
+import java.util.HashMap;
+
+/**
+ * 返回对象
+ *
+ * @author wujun
+ */
+public class Result extends HashMap<String, Object>
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 状态码 */
+    public static final String CODE_TAG = "code";
+
+    /** 返回内容 */
+    public static final String MSG_TAG = "msg";
+
+    /** 数据对象 */
+    public static final String DATA_TAG = "data";
+
+    private Boolean success = true;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public Result setSuccess(Boolean success) {
+        this.success = success;
+        return this;
+    }
+
+
+
+    /**
+     * 初始化一个新创建的 AjaxResult 对象，使其表示一个空消息。
+     */
+    public Result()
+    {
+    }
+
+    /**
+     * 初始化一个新创建的 AjaxResult 对象
+     * 
+     * @param code 状态码
+     * @param msg 返回内容
+     */
+    public Result(int code, String msg)
+    {
+        super.put(CODE_TAG, code);
+        super.put(MSG_TAG, msg);
+    }
+
+    /**
+     * 初始化一个新创建的 AjaxResult 对象
+     * 
+     * @param code 状态码
+     * @param msg 返回内容
+     * @param data 数据对象
+     */
+    public Result(int code, String msg, Object data)
+    {
+        super.put(CODE_TAG, code);
+        super.put(MSG_TAG, msg);
+        super.put(DATA_TAG, data);
+    }
+
+    public static Result getResult(int i, String s) {
+        return Result.error(i,s);
+    }
+
+    /*public static Result getResult(RespCodeMsg systemRedisBusy) {
+        return Result.error(systemRedisBusy.getCode(),systemRedisBusy.getMsg());
+    }*/
+
+    /**
+     * 方便链式调用
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    @Override
+    public Result put(String key, Object value)
+    {
+        super.put(key, value);
+        return this;
+    }
+
+    /**
+     * 返回成功消息
+     * 
+     * @return 成功消息
+     */
+    public static Result success()
+    {
+        return Result.success("success");
+    }
+    public static Result ok()
+    {
+        return Result.success("success");
+    }
+
+    /**
+     * 返回成功数据
+     * 
+     * @return 成功消息
+     */
+    public static Result ok(Object data)
+    {
+        return Result.success("success", data);
+    }
+    public static Result success(Object data)
+    {
+        return Result.success("success", data);
+    }
+
+    /**
+     * 返回成功消息
+     * 
+     * @param msg 返回内容
+     * @param data 数据对象
+     * @return 成功消息
+     */
+    public static Result success(String msg, Object data)
+    {
+        return new Result(0, msg, data);
+    }
+    public static Result success(int code, String msg, Object data)
+    {
+        return new Result(code, msg, data);
+    }
+
+    /**
+     * 返回错误消息
+     * 
+     * @return
+     */
+    public static Result fail()
+    {
+        return Result.error("fail").setSuccess(false);
+    }
+    public static Result error()
+    {
+        return Result.error("fail").setSuccess(false);
+    }
+
+    /**
+     * 返回错误消息
+     * 
+     * @param msg 返回内容
+     * @return 警告消息
+     */
+    public static Result fail(String msg)
+    {
+        return Result.error(msg, null).setSuccess(false);
+    }
+    public static Result error(String msg)
+    {
+        return Result.error(msg, null).setSuccess(false);
+    }
+
+    /**
+     * 返回错误消息
+     * 
+     * @param msg 返回内容
+     * @param data 数据对象
+     * @return 警告消息
+     */
+    public static Result error(String msg, Object data)
+    {
+        return new Result(500, msg, data).setSuccess(false);
+    }
+
+    /**
+     * 返回错误消息
+     * 
+     * @param code 状态码
+     * @param msg 返回内容
+     * @return 警告消息
+     */
+    public static Result error(int code, String msg)
+    {
+        return new Result(code, msg, null).setSuccess(false);
+    }
+}
